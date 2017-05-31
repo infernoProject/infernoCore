a332163b65f6c7f320b09294ef40af6e07d5cabb
Alexey Derygin <Alexey.Derygin@kohls.com>
2017-05-31 15:10:24 +0400

    Add ITests for realm list

--- itests/src/test/java/ru/infernoproject/tests/realmd/RealmServerTest.java
+++ itests/src/test/java/ru/infernoproject/tests/realmd/RealmServerTest.java
@@ -10,6 +10,7 @@ import ru.infernoproject.tests.AbstractIT;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -100,6 +101,71 @@ public class RealmServerTest extends AbstractIT {
         assertThat("User should not pass login challenge", response.getByte(), equalTo(RealmErrorCodes.AUTH_INVALID));
     }
 
+    @Test(groups = { "IC", "ICRS", "ICRS007" }, description = "RealmServer should return session token")
+    public void testCaseICRS007() {
+        byte[] serverSalt = getCryptoConfig();
+        ByteWrapper response;
+
+        response = registerUser(
+            "testUserICRS007", "testUserICRS007@testCase", "testPassword", serverSalt
+        );
+        assertThat("User should be registered", response.getByte(), equalTo(RealmErrorCodes.SUCCESS));
+
+        response = logInStep1("testUserICRS007");
+        assertThat("User should be able to start login challenge", response.getByte(), equalTo(RealmErrorCodes.SUCCESS));
+
+        response = logInStep2("testUserICRS007", "testPassword", serverSalt, response);
+        assertThat("User should pass login challenge", response.getByte(), equalTo(RealmErrorCodes.SUCCESS));
+
+        response = getSessionToken();
+        assertThat("User should receive session token", response.getByte(), equalTo(RealmErrorCodes.SUCCESS));
+    }
+
+    @Test(groups = { "IC", "ICRS", "ICRS008" }, description = "RealmServer should not return session token for unauthorized user")
+    public void testCaseICRS008() {
+        ByteWrapper response;
+
+        response = getSessionToken();
+        assertThat("User should not receive session token", response.getByte(), equalTo(RealmErrorCodes.AUTH_REQUIRED));
+    }
+
+    @Test(groups = { "IC", "ICRS", "ICRS009" }, description = "RealmServer should return realm list")
+    public void testCaseICRS009() {
+        byte[] serverSalt = getCryptoConfig();
+        ByteWrapper response;
+
+        response = registerUser(
+                "testUserICRS009", "testUserICRS009@testCase", "testPassword", serverSalt
+        );
+        assertThat("User should be registered", response.getByte(), equalTo(RealmErrorCodes.SUCCESS));
+
+        response = logInStep1("testUserICRS009");
+        assertThat("User should be able to start login challenge", response.getByte(), equalTo(RealmErrorCodes.SUCCESS));
+
+        response = logInStep2("testUserICRS009", "testPassword", serverSalt, response);
+        assertThat("User should pass login challenge", response.getByte(), equalTo(RealmErrorCodes.SUCCESS));
+
+        response = getRealList();
+        assertThat("User should receive realm list", response.getByte(), equalTo(RealmErrorCodes.SUCCESS));
+
+        List<ByteWrapper> realmList = response.getList();
+        assertThat("Realm list should contain 1 element", realmList.size(), equalTo(1));
+
+        ByteWrapper realmServer = realmList.get(0);
+        assertThat("Realm Server name mismatch", realmServer.getString(), equalTo("Test World"));
+        assertThat("Realm Server type mismatch", realmServer.getInt(), equalTo(1));
+        assertThat("Realm Server host mismatch", realmServer.getString(), equalTo(config.getString("world.server.host", "localhost")));
+        assertThat("Realm Server port mismatch", realmServer.getInt(), equalTo(config.getInt("world.server.port", 8085)));
+    }
+
+    @Test(groups = { "IC", "ICRS", "ICRS010" }, description = "RealmServer should not return realm list to unauthorized user")
+    public void testCaseICRS010() {
+        ByteWrapper response;
+
+        response = getRealList();
+        assertThat("User should not receive realm list", response.getByte(), equalTo(RealmErrorCodes.AUTH_REQUIRED));
+    }
+
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
         if ((testClient != null)&&testClient.isConnected()) {
@@ -108,6 +174,28 @@ public class RealmServerTest extends AbstractIT {
     }
 
     // Realm Server client methods
+
+    private ByteWrapper getRealList() {
+        try {
+            ByteWrapper response = sendRecv(new ByteArray(RealmOperations.REALM_LIST));
+
+            assertThat("Invalid OPCode", response.getByte(), equalTo(RealmOperations.REALM_LIST));
+            return response.getWrapper();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ByteWrapper getSessionToken() {
+        try {
+            ByteWrapper response = sendRecv(new ByteArray(RealmOperations.SESSION_TOKEN));
+
+            assertThat("Invalid OPCode", response.getByte(), equalTo(RealmOperations.SESSION_TOKEN));
+            return response.getWrapper();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private ByteWrapper logInStep1(String login) {
         try {
