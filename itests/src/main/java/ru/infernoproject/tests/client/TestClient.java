1d859c12a7ed7136b3afa94265b80709416a1f96
Alexey Derygin <Alexey.Derygin@kohls.com>
2017-05-31 12:17:09 +0400

    Add ITests for user sign up

--- itests/src/main/java/ru/infernoproject/tests/client/TestClient.java
+++ itests/src/main/java/ru/infernoproject/tests/client/TestClient.java
@@ -75,7 +75,7 @@ public class TestClient extends SimpleChannelInboundHandler<ByteWrapper> {
     public synchronized ByteWrapper recv(int retryCount, int timeOut) throws InterruptedException {
         for (int tryNumber = 0; tryNumber < retryCount; tryNumber++) {
             if (!receiveQueue.isEmpty())
-                return receiveQueue.peek();
+                return receiveQueue.poll();
 
             Thread.sleep(timeOut);
         }
